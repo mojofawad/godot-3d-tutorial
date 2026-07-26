078dd146a1db7751e481a7a96d8a53380f56bbbe
mojofawad <mojofawad@gmail.com>
2026-07-25 23:32:59 -0400

    spawn mobs in main scene

---
+++ 3d_squash_the_creeps_starter/Main.cs
@@ -0,0 +1,32 @@
+using Godot;
+
+namespace SquashTheCreeps3D;
+
+public partial class Main : Node
+{
+	[Export]
+	public PackedScene MobScene { get; set; }
+	
+	public override void _Ready()
+	{
+		
+	}
+	
+	public override void _Process(double delta)
+	{
+		
+	}
+
+	private void OnMobTimerTimeout()
+	{
+		var mob = MobScene.Instantiate<Mob>();
+
+		var mobSpawnLocation = GetNode<PathFollow3D>("SpawnPath/SpawnLocation");
+		mobSpawnLocation.ProgressRatio = GD.Randf();
+
+		Vector3 playerPosition = GetNode<Player>("Player").Position;
+		mob.Initialize(mobSpawnLocation.Position, playerPosition);
+		
+		AddChild(mob);
+	}
+}
